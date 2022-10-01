e4d63d55d5b67be6089dfbc4253dbf43459458bb
chiranthkothari <chiranthkothari96@gmail.com>
2022-10-01 21:51:46 +0530

    added comments for app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
-    //returns 'Hello world"
+    return 'Hello World!'; //returns hello world
   }
 }
